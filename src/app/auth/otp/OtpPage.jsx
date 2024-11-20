c6d4e4ba5bda34103005bfae56eff0fd0ad28d75
dimpal001 <dimpaldas518@gmail.com>
2024-11-20 13:30:11 +0530

    add authentication pages such as login, register, opt, forgot password, new password etc

---
+++ src/app/auth/otp/OtpPage.jsx
@@ -0,0 +1,151 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import 'animate.css'
+
+import { ArrowRight } from 'lucide-react'
+import Image from 'next/image'
+import LoginCartImage from '../../../assets/login_cart.svg'
+import { enqueueSnackbar } from 'notistack'
+import Loading from '@/app/admin_/components/Loading'
+import axios from 'axios'
+import { useRouter, useSearchParams } from 'next/navigation'
+
+export default function OtpPage() {
+  const searchParams = useSearchParams()
+  const path = searchParams.get('dvx')
+  const [otp, setOtp] = useState('')
+  const [submitting, setSubmitting] = useState(false)
+  const [checking, setChecking] = useState(true)
+
+  const router = useRouter()
+
+  useEffect(() => {
+    document.title = 'Enter OTP | Clothes2Wear'
+  }, [])
+
+  const handleCheck = async () => {
+    try {
+      const response = await axios.post(
+        '/api/auth/verify-otp/check-token',
+        {},
+        {
+          withCredentials: true,
+        }
+      )
+      if (response.status === 200) {
+        setChecking(false)
+      }
+    } catch (error) {
+      router.push('/')
+      enqueueSnackbar(error?.response?.data?.message, { variant: 'error' })
+    }
+  }
+
+  useEffect(() => {
+    handleCheck()
+  }, [])
+
+  const handleSubmit = async () => {
+    const email = localStorage.getItem('email')
+    try {
+      setSubmitting(true)
+      const response = await axios.post(
+        path ? path : '/api/auth/verify-otp',
+        {
+          email,
+          otp,
+        },
+        { withCredentials: true }
+      )
+      if (response.status === 201) {
+        enqueueSnackbar(response.data.message, { variant: 'success' })
+        router.push('/auth/new-password')
+      }
+      if (response.status === 202) {
+        enqueueSnackbar(response.data.message, { variant: 'success' })
+        router.push('/auth/full-name')
+      }
+      if (response.status === 200) {
+        enqueueSnackbar(response.data.message, { variant: 'success' })
+        router.push('/')
+      }
+    } catch (error) {
+      enqueueSnackbar(error?.response?.data?.message, { variant: 'error' })
+    } finally {
+      setSubmitting(false)
+    }
+  }
+
+  if (checking) {
+    return <Loading />
+  }
+
+  return (
+    <div className='min-h-screen flex items-center max-sm:p-4 justify-center bg-[#faf6f3]'>
+      <div
+        className={`w-full relative animate__animated animate__fadeInUp animate__faster transition-all duration-500 max-w-4xl flex md:flex-row flex-col-reverse bg-white rounded-xl shadow-lg overflow-hidden`}
+      >
+        {/* Form Section */}
+        <div className={`flex-1 p-10`}>
+          <div className='text-center md:text-left'>
+            <h1 className='text-2xl font-bold mb-4'>Logo</h1>
+            <h2 className='text-lg text-gray-500 mb-2'>Welcome back !!!</h2>
+            <h1 className='text-4xl font-extrabold text-black mb-8'>
+              Submit OTP
+            </h1>
+          </div>
+
+          <div>
+            <div className='mb-6'>
+              <label
+                htmlFor='otp'
+                className='block text-gray-600 text-sm font-medium mb-2'
+              >
+                Enter OTP
+              </label>
+              <input
+                type='otp'
+                id='otp'
+                value={otp}
+                onChange={(e) => {
+                  setOtp(e.target.value)
+                }}
+                placeholder='6 digits OTP'
+                className={`w-full px-4 py-3 bg-[#FDF3E9] rounded-lg border focus:ring-2 outline-none focus:ring-pink-300 text-gray-800`}
+              />
+            </div>
+
+            <button
+              disabled={submitting}
+              onClick={handleSubmit}
+              className={`w-full ${
+                submitting ? 'opacity-50' : 'opacity-100 hover:bg-pink-600'
+              } bg-pink-500 text-white text-lg font-medium py-3 rounded-full flex items-center justify-center gap-2`}
+            >
+              {submitting ? (
+                <>Please wait</>
+              ) : (
+                <>
+                  Submit OTP <ArrowRight className='w-5 h-5' />
+                </>
+              )}
+            </button>
+          </div>
+        </div>
+
+        {/* Illustration Section */}
+        <div className=' md:block flex-1 max-sm:p-10 max-sm:px-28 bg-[#FDF3E9] flex justify-center items-center'>
+          <div className='relative flex justify-center items-center w-full h-full'>
+            <Image
+              src={LoginCartImage}
+              width={250}
+              height={250}
+              alt='Login Image'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
