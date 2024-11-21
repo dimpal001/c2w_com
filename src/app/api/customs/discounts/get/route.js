d191c8ed60c895b9a82ce2fdfcb193bb2c231e57
dimpal001 <dimpaldas518@gmail.com>
2024-11-21 10:51:31 +0530

    remove auth middleware for public apis

--- src/app/api/customs/discounts/get/route.js
+++ src/app/api/customs/discounts/get/route.js
@@ -1,9 +1,8 @@
-import { isAdmin } from '@/app/api/middleware/adminAuth'
 import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server'
 const prisma = new PrismaClient()
 
-export async function GET(request) {
+export async function GET() {
   try {
     const discounts = await prisma.discount.findMany()
 
