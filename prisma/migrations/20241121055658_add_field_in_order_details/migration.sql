67139267a02454885f516ee482b87bfe88d096dd
dimpal001 <dimpaldas518@gmail.com>
2024-11-22 18:04:05 +0530

    add new database model

--- prisma/migrations/20241121055658_add_field_in_order_details/migration.sql
+++ prisma/migrations/20241121055658_add_field_in_order_details/migration.sql
@@ -2,5 +2,3 @@
 ALTER TABLE `OrderDetails` ADD COLUMN `notes` TEXT NULL,
     ADD COLUMN `trackingId` VARCHAR(191) NULL;
 
--- AlterTable
-ALTER TABLE `UserAddress` ADD COLUMN `fullName` VARCHAR(191) NULL;
