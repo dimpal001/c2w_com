350729417a113b3f8e61674f4929cae899e6fc7a
dimpal001 <dimpaldas518@gmail.com>
2024-12-27 15:31:38 +0530

    add warning notification on admin panel dashboard and home page

--- src/app/admin_/components/Layout.jsx
+++ src/app/admin_/components/Layout.jsx
@@ -10,6 +10,7 @@ import { enqueueSnackbar } from 'notistack'
 import Loading from './Loading'
 import authCheck from '@/utils/authCheck'
 import { useUserContext } from '@/app/context/UserContext'
+import { Modal, ModalBody, ModalHeader } from '@/app/Components/CustomModal'
 
 // eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => {
@@ -19,6 +20,8 @@ const Layout = ({ children }) => {
   const router = useRouter()
   const pathname = usePathname()
   const { user } = useUserContext()
+
+  const [showWarning, setShowWarning] = useState(false)
 
   useEffect(() => {
     const checkAuth = async () => {
@@ -66,11 +69,21 @@ const Layout = ({ children }) => {
       if (response.status === 200) {
         router.push('/admin_/login')
         enqueueSnackbar('Logout Successfull')
+        localStorage.removeItem('showWarning')
       }
     } catch {
       enqueueSnackbar('Something went wrong, try again!')
     }
   }
+
+  useEffect(() => {
+    const storedData = localStorage.getItem('showWarning')
+
+    if (!storedData) {
+      setShowWarning(true)
+      localStorage.setItem('showWarning', 'true')
+    }
+  }, [])
 
   return (
     <div className='flex bg-white flex-col text-sm min-h-screen'>
@@ -110,6 +123,34 @@ const Layout = ({ children }) => {
             )}
           </main>
         </div>
+      )}
+      {showWarning && (
+        <Modal size={'xl'} isOpen={true}>
+          <ModalHeader>
+            <span className='text-blue-800'>Welcome!</span>
+          </ModalHeader>
+          <ModalBody className='p-6 bg-gray-50 rounded-lg shadow-md'>
+            <p className='text-lg font-semibold text-gray-800 mb-4'>
+              Welcome to the platform! We&apos;re glad to have you here. Please
+              take a moment to read the following notice.
+            </p>
+            <div className='p-4 bg-red-100 border-l-4 border-red-500 text-red-700 rounded-md mb-6'>
+              <strong className='block font-bold'>Warning:</strong>
+              <span className='text-sm'>
+                To avoid being logged out of your current session, please
+                refrain from navigating to the main website in the same browser
+                session. Use a separate browser or an incognito/private window
+                if you need to access the main website.
+              </span>
+            </div>
+            <button
+              className='w-full py-3 px-4 bg-blue-800 text-white font-semibold rounded-md shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+              onClick={() => setShowWarning(false)}
+            >
+              Got it
+            </button>
+          </ModalBody>
+        </Modal>
       )}
     </div>
   )
